c5fb0a4b69f6fbe1bf0d5adb79867c5f3689fc46
GopiKrishnaK <bk492@student.aru.ac.uk>
2024-03-17 13:35:17 +0000

    fetching the data from backend api

--- src/Countries.js
+++ src/Countries.js
@@ -1,11 +1,31 @@
 import { useParams } from "react-router-dom";
 import React  from "react";
 
+import { useState, useEffect } from "react";
+
 const Countries = () => {
     const {continent} = useParams();
+    const [countries,setCountries] = useState(null);
+
+    useEffect(() =>{
+        fetch(`http://127.0.0.1:8000/${continent}/countries/`)
+            .then(res =>{
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setCountries(data);
+            });
+    },[continent]); 
     return ( 
-        <h1>{continent} is selected</h1>
-    );
+        <div>
+            <h1>{continent} is selected</h1>
+            {countries && countries.countries.map(country =>(
+                <h4>{country}</h4>
+            )
+            )}
+        </div>
+    );       
 }
  
 export default Countries;
