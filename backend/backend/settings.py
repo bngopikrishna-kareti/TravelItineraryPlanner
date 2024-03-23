220a84ba65530845fc6725b0de6214579c2b2fcc
GopiKrishnaK <bk492@student.aru.ac.uk>
2024-03-23 14:01:34 +0000

    adding API and model for country details

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'countries',
     'corsheaders',
+    'countrydetails'
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('<str:continent>/countries/', include('countries.urls'))
+    path('<str:continent>/countries/', include('countries.urls')),
+    path('touristAttractions/', include('countrydetails.urls'))
 ]
