8788c9de056f57f3068092fba60dab44b252202f
GopiKrishnaK <bk492@student.aru.ac.uk>
2024-03-25 01:42:18 +0000

    Adding maps to the application

--- src/TouristAttractions.js
+++ src/TouristAttractions.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+// import { useLocation } from "react-router-dom";
+import ReactMapGL from "react-map-gl";
 const TouristAttractions = () => {
-    const {selectedCountries} = useLocation().state;
-    const {dates} = useLocation().state;
-    const noOfDays = Math.round((dates[1].getTime()-dates[0].getTime())/(1000*3600*24))
+    // const {selectedCountries} = useLocation().state;
+    // const {dates} = useLocation().state;
+    // const noOfDays = Math.round((dates[1].getTime()-dates[0].getTime())/(1000*3600*24));
+    const [viewPort, setViewPort] = useState(
+        {
+            latitude: 45.4211,
+            longitude: -75.6903,
+            width: "100vw",
+            height: "100vh",
+            zoom: 10
+
+        }
+    );
     return (  
         <div className="touristAttractions">
             <div className="map-div">
-                <h1>Map</h1>
+                <ReactMapGL {...viewPort} 
+                mapStyle="mapbox://styles/mapbox/streets-v12"
+                mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                onMove={evt => setViewPort(evt.viewState)}
+                >
+                    markers here
+
+                </ReactMapGL>
             </div>
             
         </div>
