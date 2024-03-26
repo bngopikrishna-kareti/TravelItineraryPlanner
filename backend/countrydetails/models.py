209cad5556a389e28ef88b8b4a2c0d0c15da7f27
GopiKrishnaK <bk492@student.aru.ac.uk>
2024-03-26 01:47:05 +0000

    Adding tourist Attractions into the database

--- backend/countrydetails/models.py
+++ backend/countrydetails/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class CountryDetail(models.Model):
+    cid = models.CharField(primary_key=True, max_length=200)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    categories = models.TextField()
+    imageUrls = models.TextField()
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
+    latitude = models.DecimalField(max_digits=10, decimal_places=6)
+    country = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.title
--- backend/countrydetails/views.py
+++ backend/countrydetails/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import JsonResponse,HttpResponse
+from django.http import JsonResponse
+from countrydetails.models import CountryDetail
 
 
 # Create your views here.
 def get_tourist_attractions(request):
-    return HttpResponse(request.GET.get("countries"))
+    countries = request.GET.get("countries", ["United Kingdom"])
+    print(countries)
+    tourist_attractions = CountryDetail.objects.all().values()
+    requested_tourist_attractions = get_requested_tourist_attractions(tourist_attractions=tourist_attractions,
+                                                                      countries=countries)
+    return JsonResponse(dict(tourist_attractions=requested_tourist_attractions))
+
+
+def get_requested_tourist_attractions(tourist_attractions: list, countries: list) -> list:
+    requested_tourist_attractions = []
+    for tourist_attraction in tourist_attractions:
+        if tourist_attraction.get("country", '') in countries:
+            requested_tourist_attractions.append(tourist_attraction)
+
+    return requested_tourist_attractions
